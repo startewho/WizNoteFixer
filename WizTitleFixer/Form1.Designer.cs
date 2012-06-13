8f90e8e4afc2956b2c072b8560bb08a3fb897e33
startewho <hoolooday@gmail.com>
2012-06-13 15:11:19 +0800

    将文档框更改为下拉控件
    +可以记录更多的关键语句
    默认只更每次保存,只保存第一个.其它的可以用户手动加入.

--- WizTitleFixer/Form1.Designer.cs
+++ WizTitleFixer/Form1.Designer.cs
@@ -30,11 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WizTitleFixer));
             this.btnReplace = new System.Windows.Forms.Button();
-            this.tbParts = new System.Windows.Forms.TextBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.btnBackUp = new System.Windows.Forms.Button();
             this.btnHelp = new System.Windows.Forms.Button();
             this.lbFixedTitles = new System.Windows.Forms.ListBox();
+            this.cmbExpress = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnReplace
@@ -46,14 +46,6 @@
             this.btnReplace.Text = "Replace";
             this.btnReplace.UseVisualStyleBackColor = true;
             this.btnReplace.Click += new System.EventHandler(this.btnConnect_Click);
-            // 
-            // tbParts
-            // 
-            this.tbParts.Location = new System.Drawing.Point(12, 12);
-            this.tbParts.Name = "tbParts";
-            this.tbParts.Size = new System.Drawing.Size(384, 21);
-            this.tbParts.TabIndex = 2;
-            this.tbParts.Text = "请将替换关键词用;分开";
             // 
             // progressBar1
             // 
@@ -86,38 +78,45 @@
             // 
             this.lbFixedTitles.FormattingEnabled = true;
             this.lbFixedTitles.ItemHeight = 12;
-            this.lbFixedTitles.Location = new System.Drawing.Point(12, 39);
+            this.lbFixedTitles.Location = new System.Drawing.Point(12, 51);
             this.lbFixedTitles.Name = "lbFixedTitles";
-            this.lbFixedTitles.Size = new System.Drawing.Size(384, 208);
+            this.lbFixedTitles.Size = new System.Drawing.Size(384, 196);
             this.lbFixedTitles.TabIndex = 6;
+            // 
+            // cmbExpress
+            // 
+            this.cmbExpress.FormattingEnabled = true;
+            this.cmbExpress.Location = new System.Drawing.Point(13, 15);
+            this.cmbExpress.Name = "cmbExpress";
+            this.cmbExpress.Size = new System.Drawing.Size(383, 20);
+            this.cmbExpress.TabIndex = 7;
             // 
             // WizTitleFixer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(521, 262);
+            this.Controls.Add(this.cmbExpress);
             this.Controls.Add(this.lbFixedTitles);
             this.Controls.Add(this.btnHelp);
             this.Controls.Add(this.btnBackUp);
             this.Controls.Add(this.progressBar1);
-            this.Controls.Add(this.tbParts);
             this.Controls.Add(this.btnReplace);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "WizTitleFixer";
             this.Text = "WizTitleFixer";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnReplace;
-        private System.Windows.Forms.TextBox tbParts;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button btnBackUp;
         private System.Windows.Forms.Button btnHelp;
         private System.Windows.Forms.ListBox lbFixedTitles;
+        private System.Windows.Forms.ComboBox cmbExpress;
     }
 }
 
